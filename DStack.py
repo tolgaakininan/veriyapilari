b7e65000d29f9059d14f63e1ed08f054549b6712
taiimf <115407911+taiimf@users.noreply.github.com>
2022-10-09 16:53:55 +0300

    Add files via upload

---
+++ DStack.py
@@ -0,0 +1,27 @@
+import collections
+class Stack:
+    def __init__(self) -> None:
+        self.container=collections.deque()
+    def push(self,val):
+        self.container.append(val)
+    def pop(self):
+        return self.container.pop()
+    def peek(self):
+        return self.container[-1]
+    def isEmpty(self):
+        return len(self.container)==0
+    def size(self):
+        return len(self.container)
+def reverseString(string):
+    stack=Stack()
+    for ch in string:
+        stack.push(ch)
+    reversedString=''
+    while stack.isEmpty is False:
+        reversedString+=stack.pop()
+    return reversedString
+if __name__=='__main__':
+   print(reverseString("We will conquere COVI-19"))
+
+
+   
